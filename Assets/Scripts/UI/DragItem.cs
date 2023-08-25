af5dd447d4ca6889047d7c70156bb833ddf93e6a
Librarian Night <72402692+DoctorNight799@users.noreply.github.com>
2023-08-25 22:03:36 +0300

    DragAndDrop Done

--- Assets/Scripts/UI/DragItem.cs
+++ Assets/Scripts/UI/DragItem.cs
@@ -9,30 +9,27 @@ public class DragItem : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
 
-    private void Awake()
-    {
+    private void Awake(){
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
-    public void OnBeginDrag(PointerEventData eventData)
-    {
+    public void OnBeginDrag(PointerEventData eventData){
         canvasGroup.alpha = .6f;
         canvasGroup.blocksRaycasts = false;
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    public void OnDrag(PointerEventData eventData){
+        if (eventData.button == PointerEventData.InputButton.Left){
+            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        }
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
+    public void OnEndDrag(PointerEventData eventData){
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
     }
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        
+    public void OnPointerDown(PointerEventData eventData){
+
     }
 }
--- Assets/Scripts/UI/Slot.cs
+++ Assets/Scripts/UI/Slot.cs
@@ -5,10 +5,8 @@ using UnityEngine.EventSystems;
 
 public class Slot : MonoBehaviour, IDropHandler
 {
-    public void OnDrop(PointerEventData eventData)
-    {
-        if(eventData.pointerDrag != null)
-        {
+    public void OnDrop(PointerEventData eventData){
+        if(eventData.pointerDrag != null){
             eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
         }
     }
